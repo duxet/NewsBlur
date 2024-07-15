a84e5764dc2fde1d4648ee3aaff13a6f64cee8f8
duxet <duxetlg@gmail.com>
2024-07-15 17:19:19 +0200

    Fix Mongo migrations

--- apps/rss_feeds/migrations/0003_mongo_version_4_0.py
+++ apps/rss_feeds/migrations/0003_mongo_version_4_0.py
@@ -3,22 +3,9 @@
 from django.conf import settings
 from django.db import migrations
 
-
-def set_mongo_feature_compatibility_version(apps, schema_editor):
-    new_version = "4.0"
-    db = settings.MONGODB.admin
-    doc = db.command({"getParameter": 1, "featureCompatibilityVersion": 1})
-    old_version = doc["featureCompatibilityVersion"]["version"]
-    print(f"\n ---> Current MongoDB featureCompatibilityVersion: {old_version}")
-
-    if old_version != new_version:
-        db.command({"setFeatureCompatibilityVersion": new_version})
-        print(f" ---> Updated MongoDB featureCompatibilityVersion: {new_version}")
-
-
 class Migration(migrations.Migration):
     dependencies = [
         ("rss_feeds", "0002_remove_mongo_types"),
     ]
 
-    operations = [migrations.RunPython(set_mongo_feature_compatibility_version, migrations.RunPython.noop)]
+    operations = []
