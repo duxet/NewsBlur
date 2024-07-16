8e8453afa4160d1d8470e5b5dbd19d82268a8ccf
duxet <duxetlg@gmail.com>
2024-07-16 16:39:26 +0200

    More clean up

--- newsblur_web/entrypoint.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-if [[ -z "${TEST}" && "${TEST}" = "True"  ]]
-then echo " ---> Starting test env"
-else python3 manage.py check_db; gunicorn -c config/gunicorn_conf.py newsblur_web.wsgi:application
-fi
